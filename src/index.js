e942d235da82b2e2d3ad8d38e6994fd0c86d26d9
tsvetkovpro <285019@gmail.com>
2016-08-21 13:27:23 +0500

    структура приложения - настройка роутинга
     - обновил точку входа в приложение, ---> импортировал роутинг

--- src/index.js
+++ src/index.js
@@ -2,26 +2,13 @@
 import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
-import configureStore from './store/configureStore';
-import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
-import {loadCourses} from './actions/courseActions';
-import {loadAuthors} from './actions/authorActions';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/toastr/build/toastr.css';
-
-
-const store = configureStore(); // create an instence of our store
-
-store.dispatch(loadCourses());
-store.dispatch(loadAuthors());
 
 render(
-  <Provider store={store}>
-    <Router history={browserHistory} routes={routes}/>
-  </Provider>,
+  <Router history={browserHistory} routes={routes}/>,
   document.getElementById('app')
 );
 
