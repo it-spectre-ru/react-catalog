84a9aa58d53840168fc2f0af3ca18878627c4370
tsvetkovpro <285019@gmail.com>
2016-09-13 13:33:27 +0500

    redux - async in redux - remove inline form

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -7,23 +7,6 @@ import * as courseActions from '../../actions/courseActions';
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      course: {title: ""}
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -35,16 +18,6 @@ class CoursesPage extends React.Component {
       <div>
         <h1>Курсы</h1>
         {this.props.courses.map(this.courseRow)}
-        <h2>Добавить курс</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title} />
-
-        <input
-          type="submit"
-          value="Сохранить"
-          onClick={this.onClickSave} />
       </div>
     );
   }
