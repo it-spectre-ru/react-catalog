b0d7c3f43065abfcb2d1b78deb8a8d8b1020b176
tsvetkovpro <285019@gmail.com>
2016-09-13 13:53:44 +0500

    redux - async in redux - dispatch action on page load

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*eslint-disable import/default */
 import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
@@ -6,10 +5,12 @@ import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
+import {loadCourses} from './actions/courseActions';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
