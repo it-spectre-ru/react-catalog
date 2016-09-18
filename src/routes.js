4378cbbd76017717be9c4d4f1827f549bff4ee27
tsvetkovpro <285019@gmail.com>
2016-09-18 22:08:35 +0500

    redux - async writes in redux - populate form via map-state-to-props

--- src/routes.js
+++ src/routes.js
@@ -6,9 +6,6 @@ import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/course/CoursesPage';
 import ManageCoursePage from './components/course/ManageCoursePage';
 
-
-
-
 export default  (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
