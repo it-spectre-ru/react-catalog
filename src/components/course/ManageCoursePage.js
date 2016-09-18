575cdd11c86561feab13f34d53e2aa08d5eaec48
tsvetkovpro <285019@gmail.com>
2016-09-18 21:57:30 +0500

    redux - async writes in redux - redirect via react router context

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -28,6 +28,7 @@ class ManageCoursePage extends React.Component {
   saveCourse(event) {
     event.preventDefault();
     this.props.actions.saveCourse(this.state.course);
+    this.context.router.push('/courses');
   }
 
   render() {
@@ -47,6 +48,10 @@ ManageCoursePage.propTypes = {
   course: PropTypes.object.isRequired,
   authors: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired
+};
+
+ManageCoursePage.contextTypes = {
+  router: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
