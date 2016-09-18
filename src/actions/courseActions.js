d290a614194a1ca3af2aab85bd341dcd56d9a96c
tsvetkovpro <285019@gmail.com>
2016-09-18 18:54:53 +0500

    redux - async writes in redux - create save course thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,9 +1,16 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockAuthorApi';
 
-
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses }
+}
+
+export function createCourseSuccess(courses) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
 }
 
 export function loadCourse() {
@@ -16,6 +23,16 @@ export function loadCourse() {
   }
 }
 
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    return courseApi.saveCourse(course).then(course => {
+      course.id ? dispatch(updateCourseSuccess(course)) :
+        dispatch(createCourseSuccess(course));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
 
 
 
